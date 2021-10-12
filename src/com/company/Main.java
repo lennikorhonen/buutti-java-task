7bf848013a03aad446e1c2bca2009a922f563f9c
Lenni Korhonen <lenni.korhonen2@gmail.com>
2021-10-12 13:08:08 +0300

    Actually runs from two threads now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,55 @@
 package com.company;
 
 import java.io.*;
+import java.util.concurrent.Exchanger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exchanger<char[]> exchanger = new Exchanger<>();
+        // Start read thread
+        Thread readFile = new Thread(new CopyFile(exchanger));
+        readFile.start();
+        // Start write thread
+        Thread writeFile = new Thread(new WriteFile(exchanger));
+        writeFile.start();
+    }
+}
 
 class CopyFile implements Runnable {
+    /* private Exchanger<char[]> exchanger;
+    char[] array;
+
+    public CopyFile(char[] array, Exchanger<char[]> exchanger) {
+        this.exchanger = exchanger;
+        this.array = array;
+    }*/
+
+    Exchanger ex;
+    CopyFile(Exchanger<char[]> ex) { this.ex = ex; }
+    char[] array = new char[50];
+
     @Override
     public void run(){
-        copyFile();
+        for(int i = 0; i < 2; i++) {
+            BufferedReader reader;
+            try {
+                System.out.println(
+                        "Thread " + Thread.currentThread().getId() + " is running"
+                );
+                File file = new File("testfile.txt");
+                reader = new BufferedReader(new FileReader(file));
+                reader.read(array);
+                System.out.println("Array:" + array);
+                array = (char[]) ex.exchange(array);
+            } catch (Exception e) {
+                System.out.println("Exception is caught " + e);
+            }
+        }
     }
 
-    public char[] copyFile() {
-        char[] array = new char[100];
-        BufferedReader reader = null;
+    /*public char[] copyFile() {
+        BufferedReader reader;
         try {
             System.out.println(
                     "Thread " + Thread.currentThread().getId() + " is running"
@@ -25,17 +64,44 @@ class CopyFile implements Runnable {
             System.out.println("Exception is caught");
         }
         return array;
-    }
+    }*/
 }
 
-
 class WriteFile implements Runnable {
+    /*private Exchanger<char[]> exchanger;
+    char[] array;
+
+    public WriteFile(char[] array, Exchanger<char[]> exchanger) {
+        this.array = array;
+        this.exchanger = exchanger;
+    }*/
+    Exchanger<char[]> ex;
+    char[] array;
+
+    WriteFile(Exchanger<char[]> ex) { this.ex = ex; }
+
+
     @Override
     public void run() {
-        writeFile();
+        for(int i = 0; i < 2; i++) {
+            try {
+                System.out.println(
+                        "Thread " + Thread.currentThread().getId() + " is running"
+                );
+                array = ex.exchange(new char[50]);
+                BufferedWriter writer = new BufferedWriter(new FileWriter("outputfile.txt", true));
+                writer.write(array);
+                writer.append(' ');
+
+                writer.close();
+                System.out.println("Got:" + array);
+            } catch (Exception e) {
+                System.out.println("Exception is caught " + e);
+            }
+        }
     }
 
-    public void writeFile() {
+    /*public void writeFile() {
         try {
             System.out.println(
                     "Thread " + Thread.currentThread().getId() + " is running"
@@ -50,18 +116,5 @@ class WriteFile implements Runnable {
         }  catch (Exception e) {
             System.out.println("Exception is caught" + e);
         }
-    }
-}
-
-public class Main {
-
-    public static void main(String[] args) {
-        // Start read thread
-        Thread readFile = new Thread(new CopyFile());
-        readFile.start();
-        // Start write thread
-        Thread writeFile = new Thread(new WriteFile());
-        writeFile.start();
-        // copy();
-    }
+    }*/
 }
